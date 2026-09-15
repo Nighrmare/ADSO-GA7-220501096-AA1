01b2176f4d6be04a0f0544eaf416e3a8a030c6c3
Nighrmare <nightmarejpq@gmail.com>
2026-09-15 18:47:44 -0500

    Metodos completos de primo y esMayor

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -23,6 +23,31 @@ public class Calculadora
         return num1 / num2;
     }
 
+    public String esMayor(int num1, int num2)
+    {
+        if (num1 > num2) {
+            return num1 + " es mayor que " + num2;
+        } else if (num2 > num1) {
+            return num2 + " es mayor que " + num1;
+        } else {
+            return "Ambos números son iguales";
+        }
+    }
+
+    public boolean primo(int num1)
+    {
+        if (num1 <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= num1 / 2; i++) {
+            if (num1 % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     static void main()
     {
         Calculadora calculadora = new Calculadora();
@@ -35,5 +60,12 @@ public class Calculadora
         JOptionPane.showMessageDialog(null,"El resultado de la resta es: "+calculadora.restar(num1,num2));
         JOptionPane.showMessageDialog(null,"El resultado de la multiplicacion es: "+calculadora.multiplicar(num1,num2));
         JOptionPane.showMessageDialog(null,"El resultado de la division es: "+calculadora.dividir(num1,num2));
+        JOptionPane.showMessageDialog(null, calculadora.esMayor(num1, num2));
+
+        if (calculadora.primo(num1)) {
+            JOptionPane.showMessageDialog(null, num1 + " SÍ es un número primo");
+        } else {
+            JOptionPane.showMessageDialog(null, num1 + " NO es un número primo");
+        }
     }
 }
